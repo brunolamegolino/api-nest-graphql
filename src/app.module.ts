7c9f2592208159cae10a705ec4371f132dada1bc
bruno lino <brunolamegolino@gmail.com>
2024-01-17 04:28:03 -0300

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { AuthorsModule } from './authors/authors.module';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      autoSchemaFile: true,
+    }),
+    AuthorsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/authors/authors.resolver.ts
@@ -0,0 +1,22 @@
+import { Resolver, Query } from '@nestjs/graphql';
+
+@Resolver()
+export class AuthorsResolver {
+  constructor() {}
+
+  @Query(() => String)
+  exemplo(): string {
+    return 'Olá, Mundo!';
+  }
+
+  //   @Query((returns) => Author)
+  //   async author(@Args('id', { type: () => Int }) id: number) {
+  //     return this.authorsService.findOneById(id);
+  //   }
+
+  //   @ResolveField()
+  //   async posts(@Parent() author: Author) {
+  //     const { id } = author;
+  //     return this.postsService.findAll({ authorId: id });
+  //   }
+}
